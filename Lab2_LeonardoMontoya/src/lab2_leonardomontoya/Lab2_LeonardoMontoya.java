fd228045e3095705182eb9028a3faf65f01c3cc8
LMontoya19 <leomontoya19@hotmail.com>
2018-05-04 15:54:04 -0600

    Login y creacion de cuenta

--- Lab2_LeonardoMontoya/src/lab2_leonardomontoya/Lab2_LeonardoMontoya.java
+++ Lab2_LeonardoMontoya/src/lab2_leonardomontoya/Lab2_LeonardoMontoya.java
@@ -22,68 +22,90 @@ public class Lab2_LeonardoMontoya {
         ArrayList<notas> notas = new ArrayList<>();
         ArrayList<examen> examenes = new ArrayList<>();
         Scanner entrada = new Scanner(System.in);
-        System.out.println("1) Administracion");
-        System.out.println("2) Crear cuenta");
-        System.out.println("3) Log in");
-        System.out.println("Ingrese su opcion");
-        int opcion = entrada.nextInt();
-        switch (opcion) {
-            case 1:
-                System.out.println("Crear examen");
-                System.out.println("Revisar examenes");
-                System.out.println("Ver notas");
-                System.out.println("Modificar notas");
-                System.out.println("Ingrese su opcion");
-                int opadmin = entrada.nextInt();
-                switch (opadmin) {
-                    case 1:
-                        ArrayList<String> preguntas = new ArrayList<>();
-                        System.out.println("Ingrese la clase de la cual es el examen");
-                        String clase = entrada.next();
-                        char resp = 's';
-                        while (resp == 's') {
-                            System.out.println("Ingrese la pregunta");
-                            String pregunta = entrada.nextLine();
-                            preguntas.add(pregunta);
-                            entrada.next();
-                            System.out.println("Desea ingresar otra pregunta?s/n");
-                            resp = entrada.next().toLowerCase().charAt(0);
+        char r = 's';
+        while (r == 's') {
+            System.out.println("1) Administracion");
+            System.out.println("2) Crear cuenta");
+            System.out.println("3) Log in");
+            System.out.println("Ingrese su opcion");
+            int opcion = entrada.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Crear examen");
+                    System.out.println("Revisar examenes");
+                    System.out.println("Ver notas");
+                    System.out.println("Modificar notas");
+                    System.out.println("Ingrese su opcion");
+                    int opadmin = entrada.nextInt();
+                    switch (opadmin) {
+                        case 1:
+                            ArrayList<String> preguntas = new ArrayList<>();
+                            System.out.println("Ingrese la clase de la cual es el examen");
+                            String clase = entrada.next();
+                            char resp = 's';
+                            while (resp == 's') {
+                                System.out.println("Ingrese la pregunta");
+                                String pregunta = entrada.nextLine();
+                                preguntas.add(pregunta);
+                                entrada.next();
+                                System.out.println("Desea ingresar otra pregunta?s/n");
+                                resp = entrada.next().toLowerCase().charAt(0);
 
+                            }
+                            break;
+                    }
+                case 2:
+                    System.out.println("Ingrese el nombre");
+                    String nombre = entrada.nextLine();
+                    entrada.next();
+                    System.out.println("Ingrese el numero de cuenta");
+                    String cuenta = entrada.next();
+                    System.out.println("Ingrese año de nacimiento");
+                    int año = entrada.nextInt();
+                    System.out.println("Ingrese mes de nacimiento(numeros)");
+                    int mes = entrada.nextInt();
+                    System.out.println("Ingrese dia de nacimiento");
+                    int dia = entrada.nextInt();
+                    Date n = new Date();
+                    n.setYear(año);
+                    n.setMonth(mes);
+                    n.setDate(dia);
+                    System.out.println("Ingrese edad");
+                    String edad = entrada.next();
+                    System.out.println("Ingrese ciudad de residencia");
+                    String ciudad = entrada.nextLine();
+                    entrada.next();
+                    System.out.println("Ingrese nacionalidad");
+                    String nacionalidad = entrada.nextLine();
+                    entrada.next();
+                    System.out.println("Ingrese id");
+                    String id = entrada.next();
+                    System.out.println("Ingrese usuario");
+                    String usuario = entrada.next();
+                    System.out.println("Ingrese password");
+                    String password = entrada.next();
+                    alumnos.add(new alumno(nombre, cuenta, n, edad, ciudad, nacionalidad, id, usuario, password, notas, examenes));
+                    break;
+                case 3:
+                    boolean log = false;
+                    int aactual = 1;
+                    System.out.println("Ingrese usuario");
+                    String user = entrada.next();
+                    System.out.println("Ingrese contraseña");
+                    String pass = entrada.next();
+                    for (alumno t : alumnos) {
+                        if (user.equals(t.getUser()) && pass.equals(t.getPassword())) {
+                            log = true;
+                            aactual = alumnos.indexOf(t);
                         }
-                        break;
-                }
-            case 2:
-                System.out.println("Ingrese el nombre");
-                String nombre = entrada.nextLine();
-                entrada.next();
-                System.out.println("Ingrese el numero de cuenta");
-                String cuenta = entrada.next();
-                System.out.println("Ingrese año de nacimiento");
-                int año = entrada.nextInt();
-                System.out.println("Ingrese mes de nacimiento(numeros)");
-                int mes = entrada.nextInt();
-                System.out.println("Ingrese dia de nacimiento");
-                int dia = entrada.nextInt();
-                Date n = new Date();
-                n.setYear(año);
-                n.setMonth(mes);
-                n.setDate(dia);
-                System.out.println("Ingrese edad");
-                int edad = entrada.nextInt();
-                System.out.println("Ingrese ciudad de residencia");
-                String ciudad = entrada.nextLine();
-                entrada.next();
-                System.out.println("Ingrese nacionalidad");
-                String nacionalidad = entrada.nextLine();
-                entrada.next();
-                System.out.println("Ingrese id");
-                String id = entrada.next();
-                System.out.println("Ingrese usuario");
-                String usuario = entrada.next();
-                System.out.println("Ingrese password");
-                String password = entrada.next();
+                    }
+                    if (log == true) {
+                        System.out.println("Bienvenido" + alumnos.get(aactual).getNombre());
+                    } else {
+                        System.out.println("Usuario o contraseña incorrecta");
+                    }
+            }
         }
-
     }
 
 }
